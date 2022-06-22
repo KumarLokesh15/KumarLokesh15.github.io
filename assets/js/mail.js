8ddbde19a121a472ed845cb28d3c49ab7091c3e8
LOKI <kumarlokesh15012000@gmail.com>
2022-06-22 10:36:14 +0530

    Update mail.js

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -8,27 +8,36 @@ form.onsubmit = (e) => {
   statusTxt.innerText = "Sending your message...";
   form.classList.add("disabled");
 
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", "mail.php", true);
-  xhr.onload = () => {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      let response = xhr.response;
-      if (
-        response.indexOf("required") != -1 ||
-        response.indexOf("valid") != -1 ||
-        response.indexOf("failed") != -1
-      ) {
-        statusTxt.style.color = "red";
-      } else {
-        form.reset();
-        setTimeout(() => {
-          statusTxt.style.display = "none";
-        }, 3000);
-      }
-      statusTxt.innerText = response;
-      form.classList.remove("disabled");
-    }
-  };
-  let formData = new FormData(form);
-  xhr.send(formData);
+  // Temporary solution
+  setTimeout(() => {
+    statusTxt.innerText =
+      "Ooops! Your message could n't be delivered.\nYou can contact using the given phone number and email id.";
+    statusTxt.style.color = "red";
+  }, 2000);
+
+  // let xhr = new XMLHttpRequest();
+  // xhr.open("POST", "mail.php", true);
+  // xhr.onload = () => {
+  //   if (xhr.readyState == 4 && xhr.status == 200) {
+  //     let response = xhr.response;
+  //     if (
+  //       response.indexOf("required") != -1 ||
+  //       response.indexOf("valid") != -1 ||
+  //       response.indexOf("failed") != -1
+  //     ) {
+  //       statusTxt.style.color = "red";
+  //     } else {
+  //       form.reset();
+  //       setTimeout(() => {
+  //         statusTxt.style.display = "none";
+  //       }, 3000);
+  //     }
+  //     response =
+  //       "Ooops! Your message could n't be delivered.\nYou can contact using the given phone number and mail id.";
+  //     statusTxt.innerText = response;
+  //     form.classList.remove("disabled");
+  //   }
+  // };
+  // let formData = new FormData(form);
+  // xhr.send(formData);
 };
